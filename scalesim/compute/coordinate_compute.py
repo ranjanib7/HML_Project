331a48ac0e74b2ad9c6e468a9a0d76aebb031e6a
Ranjani <Ranjani@RANJANISLAPTOP>
2024-04-19 00:19:19 -0400

    Changes for coordinate computation continued

--- scalesim/compute/coordinate_compute.py
+++ scalesim/compute/coordinate_compute.py
@@ -36,3 +36,48 @@ class coordinate_compute_blk:
                 output_coordinates.append(output_indices)
 
         return output_coordinates
+
+    def csr_to_dense_coordinates(self, indptr, indices):
+        #data, indptr, indices = compress_sparse_matrix(matrix)
+        coordinates = []
+        row_index = 0
+        for i in range(len(indptr) - 1):
+            for j in range(indptr[i], indptr[i + 1]):
+                col_index = indices[j]
+                coordinates.append((row_index, col_index))
+            row_index += 1
+        return coordinates
+    
+    def valid_convolution_pairs(self, input_coords, weight_coords, weight_dim, input_dim):
+        valid_operations = []
+        weight_rows, weight_cols = weight_dim
+        input_rows, input_cols = input_dim
+        
+        for input_coord in input_coords:
+            input_row, input_col = input_coord
+            for weight_coord in weight_coords:
+                weight_row, weight_col = weight_coord
+                
+                # Calculate the start and end indices of the convolution window
+                conv_window_start_row = input_row - weight_row
+                conv_window_end_row = conv_window_start_row + weight_rows
+                conv_window_start_col = input_col - weight_col
+                conv_window_end_col = conv_window_start_col + weight_cols
+                
+                # Check if the convolution window fits within the input dimensions
+                if (conv_window_start_row >= 0 and conv_window_end_row <= input_rows and
+                    conv_window_start_col >= 0 and conv_window_end_col <= input_cols):
+                    valid_operations.append((input_coord, weight_coord))
+        
+        return valid_operations
+        
+    def find_convolution_output_coordinates(self, valid_pairs):
+        output_coords = []
+        for input_coord, weight_coord in valid_pairs:
+            input_row, input_col = input_coord
+            weight_row, weight_col = weight_coord
+            output_row = input_row - weight_row
+            output_col = input_col - weight_col
+            output_coords.append((output_row, output_col))
+        return output_coords
+
